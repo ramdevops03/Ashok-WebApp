fcfc9ff54bebcf3d73d854c2a4f65bf4bba7b36f
ramdevops03 <79424528+ramdevops03@users.noreply.github.com>
2023-05-10 22:37:47 -0400

    Add files via upload

---
+++ src/main/java/com/mywebapp/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.mycompany.mywebapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
